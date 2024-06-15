7fd179d8b854e71387521d01008a52a72eff12d6
DeveloperAkashx <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-15 11:42:18 +0530

    Area of Circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;	//put radius get area!!
+int main()		//area of circle=py*r*r
+{
+	float r = 10 ;
+	float a = 3.14*r*r;
+	cout<<"Area of Circle : ";
+	cout<<a<<endl;
+}
