7ecccf0e4495505e9abb0348cc603510a742b40f
DeveloperAkashx <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-15 11:41:42 +0530

    Arithmatic Operation

---
+++ arithmatic.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//	float x = 5.5 ,y = 10;
+//	cout<<x+y<<endl;
+//	cout<<x-y<<endl;
+//	cout<<x*y<<endl;
+//	cout<<x/y<<endl;
+
+//	int x=4;
+//	cout<<x<<endl;
+//	++x;
+//	cout<<x<<endl;
+//	cout<<x<<endl;
+	
+	float x=2;
+	x++;
+	cout<<x<<endl;
+	x--;
+	cout<<x<<endl;
+	cout<<++x<<endl;
+	cout<<x--<<endl;
+	cout<<x<<endl;
+}
