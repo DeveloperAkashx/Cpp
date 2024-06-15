09491ac60641e4cbeed1909ff5f04ed4cb0c5037
DeveloperAkashx <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-15 11:40:30 +0530

    Simple

---
+++ First.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout<<"Cpp World Code\n";
+//	cout<<"another code sample\n";
+//	cout<<"New Code Run";
+//}
+#include<iostream>
+using namespace std;
+int main()
+{
+//	int x = 5;
+//	int y = 10;
+//	cout<<x*y;
+	
+//	int x,y;
+//	x=40;
+//	y=60;
+//	cout<<x+y;
+	
+	int x=5;
+	cout<<x<<endl;
+	x=7;
+	cout<<x<<endl;
+	x+=10; //x=x+10;
+	cout<<x<<endl;
+	x-=30;
+	cout<<x<<endl;
+}
