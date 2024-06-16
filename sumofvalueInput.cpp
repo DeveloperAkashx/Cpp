bc0718fb472829bd337b7cbfd966f58c6fc57f20
DeveloperAkashx <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-16 11:19:33 +0530

    Sum of Value Input

---
+++ sumofvalueInput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()	// Sum of number with User Input
+{
+	int x,y;
+	cout<<"Enter the 1st value : ";
+	cin>>x;
+	cout<<"Enter the 2nd value : ";
+	cin>>y;
+	cout<<"Sum of values : "<<x+y;
+}
