f360b8d4b8fb21b4aa9930f71a97f11856f47751
DeveloperAkashx <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-16 11:22:48 +0530

    Modulus Operator

---
+++ modulus.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() // modulus operator with User Input
+{
+	int x;
+	int y;
+	cout<<"Enter the 1st value : ";
+	cin>>x;
+	cout<<"Enter the 2nd value : ";
+	cin>>y;
+	cout<<"Modulus : "<<x%y;
+}
