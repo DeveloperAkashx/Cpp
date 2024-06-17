3e6d325bd2579f907aa5bb164cfb3d9c263bbc0b
DeveloperAkashx <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-17 12:22:54 +0530

    Even Odd if else

---
+++ evenodd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the number : ";
+	cin>>n;
+	if (n % 2 == 0)
+	{
+		cout<<"This number is Even";
+	}
+	else
+	{
+		cout<<"This number is Odd";
+	}
+}
