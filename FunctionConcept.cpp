d9b728b3e598e294566c3e51449ba23bdbf3a9c4
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-23 11:30:01 +0530

    Function Concept

---
+++ FunctionConcept.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void Akash()
+{
+	cout<<"Hello! I'm the owner of This Company"<<endl;
+	cout<<"How are you Team!"<<endl;
+}
+int main()
+{
+	Akash();	// Function Call
+	cout<<"Team is Great Mr. Akash"<<endl;
+	Akash();	// Function Call
+}
