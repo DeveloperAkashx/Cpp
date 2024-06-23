22a6273a7621b6f1168a46596e0496e1180408f2
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-23 11:30:59 +0530

    Multiple Functions

---
+++ MultipleFunctions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void usa()
+{
+	cout<<"You are in USA"<<endl;
+}
+void india()
+{
+	cout<<"You are in India"<<endl;
+}
+int main()
+{
+	cout<<" Where I'm ? "<<endl;
+	india();
+	usa();
+}
