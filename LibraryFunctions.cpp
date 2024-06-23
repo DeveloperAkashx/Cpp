1ecdcd319241da54683d7f8f99e4d9054cdcd885
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-23 11:32:29 +0530

    Library Functions

---
+++ LibraryFunctions.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath> 	// Header file for Direct Calculations
+using namespace std;
+int main()				// Library function
+{
+	cout<<sqrt(16)<<endl;	 	// For Square root
+	cout<<cbrt(8)<<endl;	 	// For Cude root
+	cout<<min(5,3)<<endl;		// Minimum betweeen Two	
+	cout<<max(4,9)<<endl;		// Maximum between Two
+	cout<<pow(2,4)<<endl;		// Finding Power of value
+}
