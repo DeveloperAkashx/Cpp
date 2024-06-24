8bbe21a0901d4b05f08aec0d8400305091dff684
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-24 10:57:40 +0530

    Address

---
+++ Address.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()			// Printing address
+{
+	int x = 2 ;
+	cout<<&x<<endl;
+	// 0x78fe1c
+	int y = 2 ;
+	cout<<&y<<endl;
+	// 0x78fe1c
+	// 0x78fe18
+}
