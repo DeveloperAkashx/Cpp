4b4d9fae8d54695b9dd00f9aa2ef0749f977a2e7
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-06-26 11:34:15 +0530

    Array Concept

---
+++ ArrayConcept.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5]={3,4,5,6,7};
+	for( int i=0 ; i <= 4 ; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+//	cout<<arr[0]<<" ";
+//	cout<<arr[1]<<" ";
+//	cout<<arr[2]<<" ";
+//	cout<<arr[3]<<" ";
+//	cout<<arr[4]<<" ";
+}
