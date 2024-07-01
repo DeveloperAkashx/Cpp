9f05fa572e2214117e445bc9d3953fe181cceec6
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-07-01 19:52:51 +0530

    Print String

---
+++ PrintString.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string s = "Code more!";
+//	cout<<s;
+	int n = s.length();
+//	cout<<n;
+	for(int i=0 ; i<n ; i++)
+	{
+		cout<<s[i];
+	}
+}
