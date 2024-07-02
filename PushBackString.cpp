8716d4d806da82ce152bcf1c411de151ffa3ef7b
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-07-02 19:54:08 +0530

    Push Back String

---
+++ PushBackString.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()	// Push use when wnat to add element at last
+{
+	string s = "Akash";
+	cout<<s<<endl;
+	s.push_back('o');
+	cout<<s<<endl;
+	s.push_back('y');
+	cout<<s<<endl;
+}
