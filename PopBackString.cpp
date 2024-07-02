582119d16ddfd3af57fa092d38f6e6679d437c1b
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-07-02 19:54:51 +0530

    Pop Back String

---
+++ PopBackString.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() // Pop use when want to remove last element
+{
+	string s = "Akash";
+	cout<<s<<endl;
+	s.pop_back();
+	cout<<s<<endl;
+	s.pop_back();
+	cout<<s<<endl;
+}
