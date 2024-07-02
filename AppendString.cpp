87dba722753e3453e6ba7df2495a44f8026a0e71
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-07-02 19:55:09 +0530

    Append String

---
+++ AppendString.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() 	// Use when expand the String 
+{
+	string s = "Now its time to change Mr.";
+	cout<<s<<endl;
+	s.append(" Akash Patil");
+	cout<<s<<endl;
+	s.append(" !");
+	cout<<s<<endl;
+}
